0a5a6122756fed73a76274f4b48317977710cd1d
ADVENTISTAS\william.moura <william.moura@iatec.com>
2019-07-11 23:44:38 -0300

    remove changes

--- Desafio_BRSPG.Api/Models/Taxa.cs
+++ Desafio_BRSPG.Api/Models/Taxa.cs
@@ -9,10 +9,8 @@ namespace Desafio_BRSPG.Api.Models
         [JsonProperty(PropertyName = "Bandeira")]
         public string Bandeira { get; set; }
         [JsonProperty(PropertyName = "Debito")]
-        [JsonConverter(typeof(DecimalFormatConverter))]
         public decimal Debito { get; set; }
         [JsonProperty(PropertyName = "Credito")]
-        [JsonConverter(typeof(DecimalFormatConverter))]
         public decimal Credito { get; set; }
     }
 }
